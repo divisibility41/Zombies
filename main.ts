87fccb92f8b70b248b8895acd3f9746b911f6c7e
divisibility41 <divisibility41@gmail.com>
2024-10-10 13:54:34 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,1 @@
-let mySprite: Sprite = null
-controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    controller.moveSprite(mySprite)
-})
-game.onUpdate(function () {
-    controller.moveSprite(mySprite)
-    music.play(music.stringPlayable("- - - - - - - - ", 120), music.PlaybackMode.UntilDone)
-})
-forever(function () {
-    pause(0)
-})
+
