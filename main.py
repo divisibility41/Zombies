760a2a042faaeebcd83150913f02a2260b503f24
divisibility41 <divisibility41@gmail.com>
2024-10-10 14:09:35 -0400

    Restore '10/10/2024, 1:32:19 PM Initialize empty repo'

--- main.py
+++
@@ -1,8 +0,0 @@
-def on_player1_button_a_pressed():
-    mySprite2: Sprite = None
-    controller.player2.move_sprite(mySprite2)
-def on_player2_button_a_pressed():
-    mySprite: Sprite = None
-    controller.player2.move_sprite(mySprite)
-def on_player2_connected():
-    pass
